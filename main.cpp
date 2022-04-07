cf1deb52261e8172e1d57ceb0d896475fb12581f
Alex <alexlin@weintek.com>
2022-04-07 13:27:16 +0800

    Add unittest from udebug

--- main.cpp
+++ main.cpp
@@ -8,73 +8,72 @@
 #include <gtest/gtest.h>
 #endif
 
-int get_unadded_value(std::multimap<int, bool>& mmap)
+int get_unadded_value(std::multimap< int, bool > &mmap)
 {
-	auto it = mmap.begin();
-	while (it!=mmap.end()) {
-		if (it->second == false) {
-			it->second = true;
-			return it->first;
-		}
-		++it;
-	}
-	return -1;
+    auto it = mmap.begin();
+    while (it != mmap.end()) {
+        if (it->second == false) {
+            it->second = true;
+            return it->first;
+        }
+        ++it;
+    }
+    return -1;
 }
 
 int add_all(std::istream &is, std::ostream &os)
 {
-	std::string line;
-	std::getline(is, line);
-	int number = std::stoi(line);
+    std::string line;
+    std::getline(is, line);
+    int number = std::stoi(line);
 
-	while ((number >= 2)&&(number <= 5000)) {
+    while ((number >= 2) && (number <= 5000)) {
 
-		std::getline(is, line);
-		std::stringstream lineStream(line);
-		std::string token;
-		std::vector< int > sumvector;
-		std::multimap<int, bool> mmap;
-		int i = 0;
-		while(lineStream >> token && i < number) {
-			mmap.insert(std::pair<int, bool>(std::stoi(token), false));
-			i++;
-		}
+        std::getline(is, line);
+        std::stringstream lineStream(line);
+        std::string token;
+        std::vector< int > sumvector;
+        std::multimap< int, bool > mmap;
+        int i = 0;
+        while (lineStream >> token && i < number) {
+            mmap.insert(std::pair< int, bool >(std::stoi(token), false));
+            i++;
+        }
 
-		int first, second, sum;
+        int first, second, sum;
 
-		while(true) {
-			first = get_unadded_value(mmap);
-			if (first == -1)
-				break;
-			second = get_unadded_value(mmap);
-			if (second == -1)
-				break;
-			sum = first + second;
-			mmap.insert(std::pair<int, bool>(sum, false));
-			sumvector.push_back(sum);
-			
-		}
+        while (true) {
+            first = get_unadded_value(mmap);
+            if (first == -1)
+                break;
+            second = get_unadded_value(mmap);
+            if (second == -1)
+                break;
+            sum = first + second;
+            mmap.insert(std::pair< int, bool >(sum, false));
+            sumvector.push_back(sum);
+        }
 
-		sum =0;
-		for (auto &v : sumvector) {
-			sum = sum + v;
-		}
-		os << sum << "\n";
-		std::getline(is, line);
-		number = std::stoi(line);
-	}
-	return 0;
+        sum = 0;
+        for (auto &v : sumvector) {
+            sum = sum + v;
+        }
+        os << sum << "\n";
+        std::getline(is, line);
+        number = std::stoi(line);
+    }
+    return 0;
 }
 
 int main(int argc, char **argv)
 {
 
 #ifdef GTEST
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 #endif
 
-	add_all(std::cin, std::cout);
+    add_all(std::cin, std::cout);
 
-	return 0;
+    return 0;
 }
--- unittest.cpp
+++ unittest.cpp
@@ -4,62 +4,53 @@
 #include <sstream>
 #include "main.cpp"
 
-TEST(cryptanalysis, test_1)
+TEST(add_all, test_1)
 {
-    std::istringstream iss("3\nThis is a test.\nCount me 1 2 3 4 5.\nWow!!!! Is this question easy?\n");
+    std::istringstream iss("3\n"
+                           "6 5 4\n"
+                           "10\n"
+                           "9 5 48 2 4 5 6 3 5 4\n"
+                           "10\n"
+                           "3 4 5 4 7 2 3 8 4 5\n"
+                           "0\n");
+
     std::ostringstream oss;
 
-    cryptanalysis(iss, oss);
+    add_all(iss, oss);
 
-    EXPECT_EQ("S 7\n"
-              "T 6\n"
-              "I 5\n"
-              "E 4\n"
-              "O 3\n"
-              "A 2\n"
-              "H 2\n"
-              "N 2\n"
-              "U 2\n"
-              "W 2\n"
-              "C 1\n"
-              "M 1\n"
-              "Q 1\n"
-              "Y 1\n",
+    EXPECT_EQ("24\n"
+              "224\n"
+              "147\n",
               oss.str());
 }
 
-TEST(cryptanalysis, test_uDebug)
+TEST(add_all, test_2)
 {
-    std::ifstream input_file("test_uDebug.txt");
+    std::istringstream iss("8\n"
+                           "4 4 6 6 8 8 10 10\n"
+                           "8\n"
+                           "4 6 6 7 7 8 8 9\n"
+                           "8\n"
+                           "3 4 6 6 6 8 8 9\n"
+                           "8\n"
+                           "3 4 5 5 5 5 8 9\n"
+                           "2\n"
+                           "99999 0\n"
+                           "4\n"
+                           "99997 49999 49999 49999\n"
+                           "4\n"
+                           "49999 99998 49999 49999\n"
+                           "0\n");
     std::ostringstream oss;
 
-    cryptanalysis(input_file, oss);
+    add_all(iss, oss);
 
-    EXPECT_EQ("E 115\n"
-              "T 66\n"
-              "R 61\n"
-              "A 56\n"
-              "N 55\n"
-              "O 54\n"
-              "I 52\n"
-              "S 51\n"
-              "D 36\n"
-              "H 33\n"
-              "M 32\n"
-              "L 30\n"
-              "U 25\n"
-              "Y 22\n"
-              "C 18\n"
-              "P 17\n"
-              "F 10\n"
-              "W 10\n"
-              "B 8\n"
-              "G 8\n"
-              "V 8\n"
-              "J 4\n"
-              "X 4\n"
-              "K 3\n"
-              "Q 3\n"
-              "Z 1\n",
+    EXPECT_EQ("166\n"
+              "165\n"
+              "148\n"
+              "130\n"
+              "99999\n"
+              "499988\n"
+              "499990\n",
               oss.str());
 }
